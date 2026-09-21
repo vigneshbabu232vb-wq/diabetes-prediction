05b97db1211ede3756ca711c7b7015d7405d8f16
Vignesh Babu <vigneshbabu232vb@gmail.com>
2026-09-21 22:21:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+#pip install streamlit
+#pip install pandas
+#pip install sklearn
+
+import streamlit as st
+import pandas as pd
+from PIL import Image
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("diabetes.csv")
+
+st.title('Disabetes Checkup')
+
+st.subheader('Training Data')
+st.write(df.describe())
+
+st.subheader('Visualisation')
+st.bar_chart(df)
+
+x=df.drop(['Outcome'], axis =1)
+y=df.iloc[:,-1]
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+def user_report():
+
+    pregnancies = st.sidebar.slider('Pregnancies', 0, 17, 3)
+    glucose = st.sidebar.slider('Glucose', 0, 200, 120)
+    blood_pressure = st.sidebar.slider('Blood Pressure', 1, 122, 70)
+    skin_thickness = st.sidebar.slider('Skin Thickness', 0, 100, 20)
+    insulin = st.sidebar.slider('Insulin', 0, 846, 79)
+    bmi = st.sidebar.slider('BMI', 0.0, 67.0, 20.0)
+    diabetes_pedigree = st.sidebar.slider(
+        'Diabetes Pedigree Function', 0.0, 2.4, 0.47
+    )
+    age = st.sidebar.slider('Age', 21, 88, 33)
+
+    user_report = {
+        'Pregnancies': pregnancies,
+        'Glucose': glucose,
+        'BloodPressure': blood_pressure,
+        'SkinThickness': skin_thickness,
+        'Insulin': insulin,
+        'BMI': bmi,
+        'DiabetesPedigreeFunction': diabetes_pedigree,
+        'Age': age
+    }
+
+    report_data = pd.DataFrame(user_report, index=[0])
+
+    return report_data
+
+user_data = user_report()
+
+rf= RandomForestClassifier()
+rf.fit(x_train,y_train)
+st.subheader('Accuracy: ')
+st.write(str(accuracy_score(y_test,rf.predict(x_test))*100)+'%')
+
+user_result = rf.predict(user_data)
+st.subheader('Your Report: ')
+output=''
+if user_result[0]==0:
+    output='You are healthy'
+else:
+    output='You are not healthy'
+
+st.write(output)
+
+# C:\Users\vigne\OneDrive\Desktop\diaga\app.py
+
+
+
+
+
